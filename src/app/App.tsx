af104a121233a35bde64fd09983eb7b203b4d653
Tanya Musara <43385626+tanyamusara@users.noreply.github.com>
2026-05-14 19:47:31 +0200

    Making project boilerplate and architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,6 +9,7 @@ function App() {
 
   return (
     <>
+    <h1 className="text-3xl font-bold underline">Hello world</h1>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
